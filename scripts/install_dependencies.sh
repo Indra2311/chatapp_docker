95979b592bf476b1b9ed8764eb5fb58b52df6b78
shahnawaz-khan-pathan <39399094+shahnawaz-khan-pathan@users.noreply.github.com>
2021-05-22 12:40:41 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
    sudo rm -rf /home/ubuntu/new_chatapp/.idea
    sudo rm -rf /home/ubuntu/new_chatapp/.github
-
+   sudo rm -rf /home/ubuntu/new_chatapp/appspec.yml
 if [ -d /home/ubuntu/new_chatapp/fundoo ]; then
    sudo rm -rf /home/ubuntu/new_chatapp/fundoo
 fi
