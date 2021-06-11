12cdf8b11ebcaa25ce9cb68f523f5b54d39f5993
shahnawaz-khan-pathan <39399094+shahnawaz-khan-pathan@users.noreply.github.com>
2021-06-11 11:59:04 +0530

    Update mv.sh

--- scripts/mv.sh
+++ scripts/mv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-   sudo mv /home/ubuntu/Copycode/.idea /home/ubuntu/new_chatapp
-   sudo mv /home/ubuntu/Copycode/.github /home/ubuntu/new_chatapp
-   sudo mv /home/ubuntu/Copycode/appspec.yml /home/ubuntu/new_chatapp
-   sudo mv /home/ubuntu/Copycode/scripts /home/ubuntu/new_chatapp
-   sudo mv /home/ubuntu/Copycode/fundoo /home/ubuntu/new_chatapp
-   sudo mv /home/ubuntu/Copycode/requirements.txt /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/Chatapp/.idea /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/Chatapp/.github /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/Chatapp/appspec.yml /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/Chatapp/scripts /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/Chatapp/fundoo /home/ubuntu/new_chatapp
+   sudo mv /home/ubuntu/Chatapp/requirements.txt /home/ubuntu/new_chatapp
    
