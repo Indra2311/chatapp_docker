bc4f3e1611f226d318e8c42bb809be475f0d110c
shahnawaz-khan-pathan <39399094+shahnawaz-khan-pathan@users.noreply.github.com>
2021-06-24 11:14:35 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-   sudo rm -rf /home/ubuntu/new_chatapp/.idea
-   sudo rm -rf /home/ubuntu/new_chatapp/.github
-   sudo rm -rf /home/ubuntu/new_chatapp/appspec.yml
-   sudo rm -rf /home/ubuntu/new_chatapp/scripts
+   sudo rm -rf /home/ubuntu/new_chatapp1/.idea
+   sudo rm -rf /home/ubuntu/new_chatapp1/.github
+   sudo rm -rf /home/ubuntu/new_chatapp1/appspec.yml
+   sudo rm -rf /home/ubuntu/new_chatapp1/scripts
    
-if [ -d /home/ubuntu/new_chatapp/fundoo ]; then
-   sudo rm -rf /home/ubuntu/new_chatapp/fundoo
+if [ -d /home/ubuntu/new_chatapp1/fundoo ]; then
+   sudo rm -rf /home/ubuntu/new_chatapp1/fundoo
 fi
-if [ -f /home/ubuntu/new_chatapp/requirements.txt ]; then
-    sudo rm -rf /home/ubuntu/new_chatapp/requirements.txt
-fi    
+if [ -f /home/ubuntu/new_chatapp1/requirements.txt ]; then
+    sudo rm -rf /home/ubuntu/new_chatapp1/requirements.txt
+fi
+
+if [ -f /home/ubuntu/new_chatapp1/Frontend ]; then
+    sudo rm -rf /home/ubuntu/new_chatapp1/Frontend
+fi
+
+if [ -f /home/ubuntu/new_chatapp1/Frontend ]; then
+sudo rm -rf /home/ubuntu/new_chatapp1/Database
+fi
+if [ -f /home/ubuntu/new_chatapp1/sonar-project.properties ]; then
+sudo rm -rf /home/ubuntu/new_chatapp1/sonar-project.properties
+fi
+if [ -f /home/ubuntu/new_chatapp1/Dockerfile ]; then
+sudo rm -rf /home/ubuntu/new_chatapp1/Dockerfile
+fi
+
+
