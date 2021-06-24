03d3476438a474d5f952c165fd2c8b145b4ad31a
shahnawaz-khan-pathan <39399094+shahnawaz-khan-pathan@users.noreply.github.com>
2021-06-24 11:28:36 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,29 +3,13 @@
    sudo rm -rf /home/ubuntu/new_chatapp1/.github
    sudo rm -rf /home/ubuntu/new_chatapp1/appspec.yml
    sudo rm -rf /home/ubuntu/new_chatapp1/scripts
-   
-if [ -d /home/ubuntu/new_chatapp1/fundoo ]; then
    sudo rm -rf /home/ubuntu/new_chatapp1/fundoo
-fi
-if [ -f /home/ubuntu/new_chatapp1/requirements.txt ]; then
-    sudo rm -rf /home/ubuntu/new_chatapp1/requirements.txt
-fi
+   sudo rm -rf /home/ubuntu/new_chatapp1/requirements.txt
+   sudo rm -rf /home/ubuntu/new_chatapp1/Frontend
+   sudo rm -rf /home/ubuntu/new_chatapp1/Database
+   sudo rm -rf /home/ubuntu/new_chatapp1/sonar-project.properties
+   sudo rm -rf /home/ubuntu/new_chatapp1/Dockerfile
+   sudo rm -rf /home/ubuntu/new_chatapp1/docker-compose.yml
 
-if [ -f /home/ubuntu/new_chatapp1/Frontend ]; then
-    sudo rm -rf /home/ubuntu/new_chatapp1/Frontend
-fi
-
-if [ -f /home/ubuntu/new_chatapp1/Database ]; then
-sudo rm -rf /home/ubuntu/new_chatapp1/Database
-fi
-if [ -f /home/ubuntu/new_chatapp1/sonar-project.properties ]; then
-sudo rm -rf /home/ubuntu/new_chatapp1/sonar-project.properties
-fi
-if [ -f /home/ubuntu/new_chatapp1/Dockerfile ]; then
-sudo rm -rf /home/ubuntu/new_chatapp1/Dockerfile
-fi
-if [ -f /home/ubuntu/new_chatapp1/docker-compose.yml ]; then
-sudo rm -rf /home/ubuntu/new_chatapp1/docker-compose.yml
-fi
 
 
