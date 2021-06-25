2eecafeffa42f56c1021565d8f1ceb3dc1c41dde
shahnawaz-khan-pathan <39399094+shahnawaz-khan-pathan@users.noreply.github.com>
2021-06-25 10:25:17 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-  sudo docker-compose down
+  
+  sudo docker rm -f frontend backend mydb
+  sudo docker rmi -f  backend_image frontend_image mysql_db base_image
+  
+  cd /home/ubuntu/new_chatapp1
+  sudo docker build -t base_image .
+  #sudo docker-compose down
+  cd /home/ubuntu/new_chatapp1/scripts
   sudo docker-compose up --build -d 
