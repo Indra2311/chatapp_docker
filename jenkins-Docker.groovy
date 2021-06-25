e4ee0207d059cff5987ee5213625ff1205cb5bc0
shahnawaz-khan-pathan <39399094+shahnawaz-khan-pathan@users.noreply.github.com>
2021-06-25 10:57:49 +0530

    Create jenkins-Docker.groovy

---
+++ jenkins-Docker.groovy
@@ -0,0 +1,32 @@
+pipeline {				//indicate the job is written in Declarative Pipeline
+    agent any				//agent specifies where the pipeline will execute. 
+    stages {
+        stage ("Pull code") {		//an arbitrary stage name
+            steps {
+                git branch: 'main', credentialsId: '0226f1a1-5b16-4ec6-a153-e0bfb1622547', url: 'https://github.com/shahnawaz-khan-pathan/new_chatapp1.git'
+            }
+        
+        }
+        
+
+
+        stage ("build") {		//an arbitrary stage name
+            steps {
+                sh 'rsync -r -e  "ssh -i /home/ubuntu/key.pem" /var/lib/jenkins/workspace/Docker-chatapp ubuntu@10.0.1.239:/home/ubuntu/'	//this is where we specify which job to invoke.
+                           
+            
+            }
+
+
+        }
+    stage ("Deploy") {		//an arbitrary stage name
+            steps {
+                sh 'ssh -i /home/ubuntu/key.pem ubuntu@10.0.1.239 "bash /home/ubuntu/Docker-chatapp/scripts/install.sh"'
+                sh 'ssh -i /home/ubuntu/key.pem ubuntu@10.0.1.239 "bash /home/ubuntu/Docker-chatapp/scripts/mv.sh"'	//this is where we specify which job to invoke.
+                sh 'ssh -i /home/ubuntu/key.pem ubuntu@10.0.1.239 "bash /home/ubuntu/new_chatapp1/scripts/start.sh"'             
+                
+            }
+            
+        }    
+    }
+}
